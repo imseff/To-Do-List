3382160b1c19294f8ab80644fa6dcce53de708bd
imseff <stephenc.solano@gmail.com>
2026-01-31 11:43:06 +0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,76 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+
+interface Task {
+  newTitle: string;
+  newContent: string;
+}
 
 export default function Home() {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [task, setTask] = useState<Task[]>([])
+
+  const buttons = [
+    {
+      label: "Edit", 
+      color: "bg-blue-700 border-blue-900"
+    },
+    {
+      label: "Delete",
+      color: "bg-red-700 border-red-900"
+    }
+  ]
+
+
+  function handleAddTask(){
+    const newTask: Task = {
+      newTitle: title,
+      newContent: content 
+    }
+    setTask(prev => [...prev, newTask]);
+    setTitle('')
+    setContent('')
+  }
+
+  function handleDeleteTask(indexToDelete: number){
+    setTask(prevTask => prevTask.filter((_, index) => index !== indexToDelete))
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex justify-center w-screen h-screen text-black">
+      <div className="w-full h-full max-w-[1440] flex flex-col justify-center items-center bg-rose-50">  
+        <div className="w-full max-w-[500] border-3 rounded-xl border-black p-3 h-[720]">
+          <div className="flex flex-col mb-7">
+            <div className="font-bold text-lg mb-2">Create New Item</div>
+            <div className="flex flex-col gap-2 items-start">
+              <input type="text" placeholder="Title" value={title} className="w-full border-2 rounded-md p-1 focus:outline-none focus:ring-1 focus:ring-rose-200 focus:border-rose-400" onChange={(e) => setTitle(e.target.value)}/>
+              <input type="text" placeholder="Content" value={content} className="w-full border-2 rounded-md p-1 focus:outline-none focus:ring-1 focus:ring-rose-200 focus:border-rose-400"onChange={(e) => setContent(e.target.value)} />
+              <button type="submit" className="bg-green-700 text-white border-green-900 border-2 px-3 py-1 rounded-xl mt-2" onClick={handleAddTask}>Add Item</button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 ">
+            <div className="font-bold text-lg mb-2">
+              Item List
+            </div>
+            {task.map((item,index) => (
+              <div key={index} className="flex justify-between p-2 border-2 rounded-md">
+                <div className="flex flex-col">
+                  <span className="font-bold">{item.newTitle}</span>
+                  <span className="text-sm">{item.newContent}</span>
+                </div>
+                <div className="flex items-center gap-1">
+                  {buttons.map((btn, btnIndex)=> (
+                    <button key={btnIndex} className={`${btn.color} text-white border-2 px-3 py-1 rounded-xl`}>
+                      {btn.label}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
