973ca0a2dd46d5db7c8b9381f7d34e6523c34b7b
im_seff <stephenc.solano@gmail.com>
2026-01-31 23:59:18 +0800

    near completion

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 interface Task {
-  newTitle: string;
+  newTitle:  string;
   newContent: string;
 }
 
@@ -10,15 +10,19 @@ export default function Home() {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [task, setTask] = useState<Task[]>([])
-
+  const [editIndex, setEditIndex] = useState(-1)
+  const [editTitle, setEditTitle] = useState('')
+  const [editContent, setEditContent] = useState('')
   const buttons = [
     {
       label: "Edit", 
-      color: "bg-blue-700 border-blue-900"
+      color: "bg-blue-700 border-blue-900",
+      onclick: (index: number) => handleEditTask(index)
     },
     {
       label: "Delete",
-      color: "bg-red-700 border-red-900"
+      color: "bg-red-700 border-red-900",
+      onclick: (index: number) => handleDeleteTask(index)
     }
   ]
 
@@ -38,6 +42,26 @@ export default function Home() {
 
   function handleDeleteTask(indexToDelete: number){
     setTask(prevTask => prevTask.filter((_, index) => index !== indexToDelete))
+    setEditIndex(-1);
+  }
+
+  function handleEditTask(indexToEdit: number){
+    setEditIndex(indexToEdit);
+    setEditTitle(task[indexToEdit].newTitle);
+    setEditContent(task[indexToEdit].newContent);
+    
+  }
+
+  function handleSaveEditTask(indexToSave: number){
+    const updatedItems = {
+      newTitle: editTitle,
+      newContent: editContent
+    }
+
+    setTask(prevTask => prevTask.map((item, index) => 
+      index === indexToSave ? updatedItems : item
+    ));
+    setEditIndex(-1);
   }
 
   return (
@@ -59,12 +83,22 @@ export default function Home() {
             {task.map((item,index) => (
               <div key={index} className="flex justify-between p-2 border-2 rounded-md">
                 <div className="flex flex-col">
-                  <span className="font-bold">{item.newTitle}</span>
-                  <span className="text-sm">{item.newContent}</span>
+                  {editIndex === index ? (
+                    <div className="flex flex-col gap-2 ">
+                      <input type="text" placeholder="Edit title" value={editTitle}  className="w-full border rounded-md p-1 focus:outline-none focus:ring-1 focus:ring-rose-200 focus:border-rose-400" onChange={(e) => setEditTitle(e.target.value)}/>
+                      <input type="text" placeholder="Edit content" value={editContent} className="w-full border rounded-md p-1 focus:outline-none focus:ring-1 focus:ring-rose-200 focus:border-rose-400"onChange={(e) => setEditContent(e.target.value)} />
+                      <button type="submit" className="bg-green-700 text-white border-green-900 border-2 px-3 py-1 rounded-xl w-fit" onClick={() => handleSaveEditTask(index)}>Save</button>
+                    </div>
+                  ):(
+                   <>
+                     <span className="font-bold">{item.newTitle}</span>
+                      <span className="text-sm">{item.newContent}</span>
+                   </>
+                  )}
                 </div>
                 <div className="flex items-center gap-1">
                   {buttons.map((btn, btnIndex)=> (
-                    <button key={btnIndex} className={`${btn.color} text-white border-2 px-3 py-1 rounded-xl`}>
+                    <button key={btnIndex} onClick={() => btn.onclick?.(index)} className={`${btn.color} text-white border-2 px-3 py-1 rounded-xl`}>
                       {btn.label}
                     </button>
                   ))}
